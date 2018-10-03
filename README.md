0350d0a1b514650561ce111154add128f5495f01
Logan Kelly <lrkelly@spscommerce.com>
2018-10-03 15:45:17 -0400

    reorganized, added requirements, fixed favico error, readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# I want to go fast.
+
+Python has become my language of choice, but many of the popular frameworks do not support async yet.  After learning 
+development using Node and Express, this is unacceptable.  My goal is to explore the async frameworks in order
+to make use of them in my Python development.
--- sanic_app/__init__.py
+++ sanic_app/__init__.py
@@ -1,9 +1,17 @@
+import os
 from sanic import Sanic
 from sanic.response import json
+
+STATIC_FOLDER = os.path.join(os.path.dirname(__file__), 'static')
+
+# the static file serving is only used during debugging
+
 
 
 def create_app():
     app = Sanic()
+    app.static('/static', STATIC_FOLDER)
+    app.static('/favicon.ico', os.path.join(STATIC_FOLDER, 'img', 'favicon.ico'))
 
     @app.route('/')
     async def test(request):
